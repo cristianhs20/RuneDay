3e28127225256a27eb7be432faa060cab4ef9480
Christian Hoyos <cristianhoyossoto@gmail.com>
2026-09-18 23:31:37 -0500

    feat: promote modular v3 character renderer

--- app/Domain/Game/Enums/ItemSlot.php
+++ app/Domain/Game/Enums/ItemSlot.php
@@ -6,8 +6,14 @@ enum ItemSlot: string
 {
     case Weapon = 'weapon';
     case Head = 'head';
+    case Neck = 'neck';
+    case Shoulder = 'shoulder';
     case Chest = 'chest';
+    case Hands = 'hands';
+    case Waist = 'waist';
+    case Legs = 'legs';
     case Feet = 'feet';
     case Back = 'back';
+    case Offhand = 'offhand';
     case Accessory = 'accessory';
 }
